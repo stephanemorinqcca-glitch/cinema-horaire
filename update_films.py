4306dd1797058db0c4f8bd135cdb2167783b76cc
stephanemorinqcca-glitch <stephane.morin.qc.ca@gmail.com>
2025-09-01 13:14:10 -0400

    Update update_films.py

--- update_films.py
+++ update_films.py
@@ -1,3 +1,6 @@
+import subprocess
+import sys
+
 # Installer les dépendances automatiquement
 subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", "requirements.txt"])
 
@@ -5,9 +8,6 @@ import requests
 import json
 from datetime import datetime
 from dateutil.parser import parse
-
-import subprocess
-import sys
 
 API_URL = "https://api.us.veezi.com/v1/sessions"
 SITE_TOKEN = "shrfm72nvm2zmr7xpsteck6b64"
